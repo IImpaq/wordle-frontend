986dadc8babce680730a9491c48c905a53d9c4ec
Marcus Gugacs <iimpaq@proton.me>
2022-07-19 16:31:10 +0200

    Added basic game logic

---
+++ src/routes/Key.svelte
@@ -0,0 +1,59 @@
+<script>
+  export let letter;
+  export let status;
+  export let pressed;
+  let hovered;
+</script>
+
+<div
+      class="key-box {pressed ? 'selected' : ''} {hovered ? 'selected' : ''}"
+      class:correct={status === "correct"}
+      class:wrong-position={status === "position"}
+      class:incorrect={status === "incorrect"}
+      on:mouseover={() => hovered = true}
+      on:mouseleave={() => hovered = false}
+>
+  <p class="key">
+    {letter}
+  </p>
+</div>
+
+<style>
+  .key-box {
+    flex: 0 0 calc(12%);
+    background-color: #fff;
+    aspect-ratio: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0;
+    margin: 0.25rem;
+    cursor: pointer;
+    box-shadow: 0 0 10px #ddd;
+  }
+
+  .key {
+    margin: 0;
+    font-weight: bold;
+    font-size: 1rem;
+  }
+
+  .selected {
+    background-color: #eee;
+  }
+
+  .correct {
+    background-color: #197278;
+    color: #fff;
+  }
+
+  .wrong-position {
+    background-color: #ffa62b;
+    color: #fff;
+  }
+
+  .incorrect {
+    background-color: #c44536;
+    color: white;
+  }
+</style>
