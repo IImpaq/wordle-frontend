a42aaf3b7e7fe4b3e0cc64fff9532e67c1b0a00f
Marcus Gugacs <iimpaq@proton.me>
2022-07-24 18:05:16 +0200

    Exported data to store

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -11,6 +11,16 @@
   import Cookies from "./Cookies.svelte";
   import Navbar from "./Navbar.svelte";
   import {onMount} from "svelte";
+  import {
+    letter_count,
+    word_count,
+    key_letters,
+    words, chosen_word,
+    current_letter,
+    current_word,
+    current_row,
+    correct_letters
+  } from "../lib/stores/game_data.js";
 
   const { addNotification } = getNotificationsContext();
   const client = new ApolloClient({
@@ -34,32 +44,20 @@
     }
   `;
 
-  const gameboard_size = 5 * 6;
-  const key_letters = [
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-  ];
-
-  let words = [];
-  let chosen_word = "";
-
-  let current_letter = 0;
-  let current_row = 0;
-  let current_word = [];
-  let correct_letters = 0;
+  const gameboard_size = $letter_count * $word_count;
 
   let keys = [];
   let letters = [];
 
   const getWord = async () => {
     await client.query({ query: GET_WORD }).then(res => {
-      chosen_word = res.data.getWord.text;
+      chosen_word.set(res.data.getWord.text);
     });
   }
 
   const getWords = async () => {
     await client.query({ query: GET_WORDS }).then(res => {
-      words = res.data.getWords;
+      words.set(res.data.getWords);
     });
   }
 
@@ -69,13 +67,13 @@
   })
 
   const jiggleLetters = () => {
-    for(let i = current_row * 5; i < (current_row * 5) + 5; i++) {
+    for(let i = $current_row * 5; i < ($current_row * 5) + 5; i++) {
       letters[i].animate("wiggle", 25*i, 650);
     }
   }
 
   const scaleLetters = () => {
-    letters[current_row * 5 + current_letter].animate("scale", 0, 250);
+    letters[$current_row * 5 + $current_letter].animate("scale", 0, 250);
   }
 
   const handleKeypress = (event) => {
@@ -90,9 +88,10 @@
 
     // WHEN PRESSING BACKSPACE
     if(key === "Backspace") {
-      if(current_letter > 0) {
-        current_word.pop();
-        letters[--current_letter + (current_row * 5)].setLetter("");
+      if($current_letter > 0) {
+        $current_word.pop();
+        $current_letter--;
+        letters[$current_letter + ($current_row * 5)].setLetter("");
         scaleLetters();
       } else {
         console.log("nothing to delete");
@@ -110,7 +109,7 @@
     // WHEN PRESSING ENTER
     if(key === "Enter") {
       // HANDLE ENTER
-      if(current_letter <= 4) {
+      if($current_letter <= 4) {
         console.log("word is not long enough");
         addNotification({
           text: "Word isn't long enough",
@@ -123,11 +122,11 @@
       }
 
       let current_word_string = "";
-      current_word.forEach(letter => {
+      $current_word.forEach(letter => {
         current_word_string += letter;
       });
 
-      if(!words.includes(current_word_string)) {
+      if(!$words.includes(current_word_string)) {
         console.log("not included");
         addNotification({
           text: "Word is unknown",
@@ -139,7 +138,7 @@
         return;
       }
 
-      if(chosen_word.length !== current_word_string.length) {
+      if($chosen_word.length !== current_word_string.length) {
         console.log("impossible word length");
         addNotification({
           text: "Impossible word length",
@@ -151,36 +150,36 @@
       }
 
       for(let i = 0; i < 5; i++) {
-        if(chosen_word[i] === current_word_string[i])
+        if($chosen_word[i] === current_word_string[i])
         {
-          letters[i + (current_row * 5)].setStatus("correct");
-          correct_letters++;
+          letters[i + ($current_row * 5)].setStatus("correct");
+          $correct_letters++;
           continue;
         }
 
-        if(chosen_word.includes(current_word_string[i]))
-          letters[i + (current_row * 5)].setStatus("position");
+        if($chosen_word.includes(current_word_string[i]))
+          letters[i + ($current_row * 5)].setStatus("position");
         else
-          letters[i + (current_row * 5)].setStatus("incorrect");
+          letters[i + ($current_row * 5)].setStatus("incorrect");
 
         for(let j = 0; j < keys.length; j++) {
           if(keys[j].getLetter() !== current_word_string[i])
             continue;
 
-          if(chosen_word[i] === current_word_string[i])
+          if($chosen_word[i] === current_word_string[i])
           {
             keys[j].setStatus("correct");
             continue;
           }
 
-          if(chosen_word.includes(current_word_string[i]))
+          if($chosen_word.includes(current_word_string[i]))
             keys[j].setStatus("position");
           else
             keys[j].setStatus("incorrect");
         }
       }
 
-      if(correct_letters === 5) {
+      if($correct_letters === 5) {
         console.log("You found out the correct word");
         addNotification({
           text: "You found out the correct word!",
@@ -188,25 +187,25 @@
           type: "success",
           removeAfter: 3000
         });
-      } else if(current_row === 5) {
-        console.log("You failed to find the correct word (" + chosen_word + ")");
+      } else if($current_row === 5) {
+        console.log("You failed to find the correct word (" + $chosen_word + ")");
         addNotification({
-          text: "You lost! The correct word is: " + chosen_word,
+          text: "You lost! The correct word is: " + $chosen_word,
           position: "top-right",
           type: "danger",
           removeAfter: 3000
         });
       }
 
-      current_word = [];
-      current_row++;
-      current_letter = 0;
-      correct_letters = 0;
+      current_word.set([]);
+      $current_row++;
+      current_letter.set(0);
+      correct_letters.set(0);
 
       return;
     }
 
-    if(!key_letters.includes(key.toUpperCase())) {
+    if(!$key_letters.includes(key.toUpperCase())) {
       console.log("pressed unknown key");
       addNotification({
         text: "Pressed an unknown key",
@@ -217,7 +216,7 @@
       return false;
     }
 
-    if(current_letter > 4)  {
+    if($current_letter > 4)  {
       console.log("max word length reached");
       addNotification({
         text: "Max word length reached",
@@ -229,10 +228,10 @@
       return;
     }
 
-    current_word.push(...key.toUpperCase());
-    letters[current_letter + (current_row * 5)].setLetter(key.toUpperCase());
+    $current_word.push(...key.toUpperCase());
+    letters[$current_letter + ($current_row * 5)].setLetter(key.toUpperCase());
     scaleLetters();
-    current_letter++;
+    current_letter.set($current_letter + 1);
   };
 
   const handleKeyrelease = (event) => {
@@ -266,7 +265,7 @@
 
   <div class="keyboard">
     <div class="keys">
-      {#each key_letters as key_letter, i}
+      {#each $key_letters as key_letter, i}
         <Key
               letter={key_letter}
               status={"unknown"}
